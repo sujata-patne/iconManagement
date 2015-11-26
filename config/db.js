d90d54173cdbdb782f126c307991224a641b5a8d
sujata-patne <sujata.patne@gmail.com>
2015-11-26 13:48:44 +0530

    Billing Gateway code removed

--- config/db.js
+++ config/db.js
@@ -7,23 +7,11 @@ var config = require('../config')();
 
 var poolCluster = mysql.createPoolCluster();
 // add configurations
-poolCluster.add('PLAN',{
-    host: config.db_host_ikon_plan,
-    user: config.db_user_ikon_plan,
-    password: config.db_pass_ikon_plan,
-    database: config.db_name_ikon_plan
-});
+
 poolCluster.add('CMS', {
     host: config.db_host_ikon_cms,
     user: config.db_user_ikon_cms,
     password: config.db_pass_ikon_cms,
     database: config.db_name_ikon_cms
 });
-poolCluster.add('GATEWAY', {
-    host: config.db_host_billing_gateway,
-    user: config.db_user_billing_gateway,
-    password: config.db_pass_billing_gateway,
-    database: config.db_name_billing_gateway
-});
-
 exports.pool = poolCluster;
